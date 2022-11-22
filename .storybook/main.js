345b18ebde842d6d41d63cb10d4ccb8e058fa893
cistT <cist.b220.t.chitose@gmail.com>
2022-11-22 13:06:03 +0900

    scssの変数を利用可能にした

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,39 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-scss',
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  features: {
+    emotionAlias: true,
+  },
+  typescript: { reactDocgen: false },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../src'),
+      }),
+    ]
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.resolve(__dirname, '../src'),
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    }
+    return config
   },
 }
