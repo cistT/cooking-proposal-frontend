def5790242932af3f7f3a728e24da87654dd4398
cistT <cist.b220.t.chitose@gmail.com>
2022-12-05 09:26:50 +0900

    全ページにヘッダーを表示するように変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
+import Header from 'components/Header/Header'
+
+import 'modern-css-reset/dist/reset.min.css'
+
+// eslint-disable-next-line require-jsdoc
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
