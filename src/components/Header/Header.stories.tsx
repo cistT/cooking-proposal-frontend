c4931ca00e64d4705dbe43ab45d7e8419a250bff
cistT <cist.b220.t.chitose@gmail.com>
2022-12-05 09:26:27 +0900

    ヘッダーの作成(要修正)

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
 
-import Header from "./Header";
+import Header from './Header'
 
 export default {
-    title: "Header",
-    component: Header,
-} as ComponentMeta<typeof Header>;
+  title: 'Header',
+  component: Header,
+} as ComponentMeta<typeof Header>
 
-export const Template: ComponentStory<typeof Header> = () => <Header />;
+export const Template: ComponentStory<typeof Header> = () => <Header />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import {
+  Header as MantineHeader,
+  Container,
+  Text,
+  Group,
+  Button,
+} from '@mantine/core'
+import React, { useState } from 'react'
+
+import styles from './Header.module.scss'
 
 const Header = () => {
-    return <div>Header</div>;
-};
+  const [isLogined, setIsLogined] = useState<boolean>(false)
+  const onClickLoginButton = () => setIsLogined(!isLogined)
 
-export default Header;
+  return (
+    <MantineHeader height={45}>
+      <Container className={styles['container']}>
+        <Text
+          variant='gradient'
+          gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+          sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+          ta='center'
+          fz='xl'
+          fw={700}
+        >
+          レシピ提供アプリ(仮)
+        </Text>
+        <Group className={styles['group']}>
+          <Button variant='white' className={styles['news-button']}>
+            お知らせ
+          </Button>
+          {isLogined ? (
+            <Button
+              variant='white'
+              onClick={onClickLoginButton}
+              className={styles['login-button']}
+            >
+              ログイン
+            </Button>
+          ) : (
+            <Button
+              variant='white'
+              onClick={onClickLoginButton}
+              className={styles['login-button']}
+            >
+              ログアウト
+            </Button>
+          )}
+          <Button variant='white' className={styles['setting-button']}>
+            設定
+          </Button>
+        </Group>
+      </Container>
+    </MantineHeader>
+  )
+}
+
+export default Header
