74a901a3a38ce607f44517a59ace91c3869dfc38
cistT <cist.b220.t.chitose@gmail.com>
2022-12-19 11:01:31 +0900

    料理のリストを楽天API経由で取得する関数

---
+++ src/features/Cook/fetchRecipeList.ts
@@ -0,0 +1,10 @@
+const fetchRecipeList = async (categoryId: string) => {
+  const res = await fetch(
+    `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=${process.env.APPLICATION_ID}&categoryId=${categoryId}`,
+  )
+  const recipeList = res.json()
+
+  return recipeList
+}
+
+export default fetchRecipeList
