68892c10960fa26c50934f93ec702c23bf006c24
cistT <cist.b220.t.chitose@gmail.com>
2022-12-22 17:06:09 +0900

    各コンテンツへ誘導するトップ画面(/)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
+import { IconSearch } from '@tabler/icons'
+import { IconNews } from '@tabler/icons'
+import { IconAlertCircle } from '@tabler/icons'
+
+import styles from './index.module.scss'
+
+import LinkTextCardWithIcon from 'components/Button/LinkTextCardWithIcon/LinkTextCardWithIcon'
+
 const Home = () => {
-  return <>test</>
+  return (
+    <div className={styles['container']}>
+      <LinkTextCardWithIcon
+        text='料理を見る'
+        href='/proposal'
+        icon={<IconSearch className={styles['icon']} />}
+      />
+
+      {/* ログイン機能及びデータベースを構築したのち追加
+      <LinkTextCardWithIcon
+        text='お気に入り'
+        href='/save'
+        icon={<IconStar className={styles['icon']} />}
+      /> */}
+
+      <LinkTextCardWithIcon
+        text='お知らせ'
+        href='/news'
+        icon={<IconNews className={styles['icon']} />}
+      />
+
+      <LinkTextCardWithIcon
+        text='利用上の注意'
+        href='/caution'
+        icon={<IconAlertCircle className={styles['icon']} />}
+      />
+    </div>
+  )
 }
 
 export default Home
