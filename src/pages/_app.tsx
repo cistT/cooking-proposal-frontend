76f0fcbdd07ae5f9567656b31edb5f698c71a499
cistT <cist.b220.t.chitose@gmail.com>
2022-12-22 17:02:35 +0900

    固定レイアウトの表示とお知らせの記事を管理するコンポーネント

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,31 @@
 import type { AppProps } from 'next/app'
-
-import Header from 'components/Header/Header'
+import { createContext, useState } from 'react'
 
 import 'modern-css-reset/dist/reset.min.css'
 
+import Layout from 'components/Layout/Layout'
+import { Contents } from 'features/News/News'
+
+export const NewsContentsContext = createContext<Contents[] | null>(null)
+
+export const NewsContext = createContext<(contents: Contents[]) => void>(
+  (contents) => undefined,
+)
+
 // eslint-disable-next-line require-jsdoc
 export default function App({ Component, pageProps }: AppProps) {
+  const [newsContents, setNewsContents] = useState<Contents[] | null>(null)
+  const temporarilySavedNewsContents = (contents: Contents[]) => {
+    setNewsContents(contents)
+  }
+
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <NewsContentsContext.Provider value={newsContents}>
+      <NewsContext.Provider value={temporarilySavedNewsContents}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NewsContext.Provider>
+    </NewsContentsContext.Provider>
   )
 }
