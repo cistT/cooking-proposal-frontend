85746407de3563e6d0229a302484b8106612cd55
cistT <cist.b220.t.chitose@gmail.com>
2022-12-22 17:31:49 +0900

    料理のテストデータ

---
+++ src/data/testData.ts
@@ -0,0 +1,116 @@
+const testData = [
+  {
+    foodImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/5bab30010b628b9d02a78e4673f273dd1bf3d4d2.86.2.3.2.jpg',
+    mediumImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/5bab30010b628b9d02a78e4673f273dd1bf3d4d2.86.2.3.2.jpg?thum=54',
+    nickname: '我家の☆毎日ゴハン',
+    pickup: 1,
+    rank: '1',
+    recipeCost: '300円前後',
+    recipeDescription:
+      '香ばしくて甘いお芋が、レンジを使うので手早く食べられますよ♪',
+    recipeId: 1010004368,
+    recipeIndication: '約10分',
+    recipeMaterial: ['さつま芋', '油', '砂糖', '塩'],
+    recipePublishday: '2012/01/28 02:59:44',
+    recipeTitle: 'すぐ食べたい時に！ さつまいもスティック♡',
+    recipeUrl: 'https://recipe.rakuten.co.jp/recipe/1010004368/',
+    shop: 0,
+    smallImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/5bab30010b628b9d02a78e4673f273dd1bf3d4d2.86.2.3.2.jpg?thum=55',
+  },
+  {
+    foodImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/66fc7af1b72e6955647b36f1434950cde8a37e14.26.2.3.2.jpg',
+    mediumImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/66fc7af1b72e6955647b36f1434950cde8a37e14.26.2.3.2.jpg?thum=54',
+    nickname: 'ななじまる',
+    pickup: 1,
+    rank: '2',
+    recipeCost: '300円前後',
+    recipeDescription:
+      '食べだすと、とまりません･:*+٩(๑❛ᴗ❛๑)۶.:里芋に片栗粉をまぶして揚げたものを、甘辛たれで絡めて仕上げました♪',
+    recipeId: 1950012560,
+    recipeIndication: '約30分',
+    recipeMaterial: [
+      '里芋',
+      '片栗粉',
+      '●醤油',
+      '●砂糖',
+      '●みりん',
+      '●酢',
+      '揚げ油',
+    ],
+    recipePublishday: '2017/01/24 12:52:49',
+    recipeTitle: '食べだすと、とまらない☆揚げ里芋の甘辛！',
+    recipeUrl: 'https://recipe.rakuten.co.jp/recipe/1950012560/',
+    shop: 0,
+    smallImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/66fc7af1b72e6955647b36f1434950cde8a37e14.26.2.3.2.jpg?thum=55',
+  },
+  {
+    foodImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/fbd7dd260d736654532e6c0b1ec185a0cede8675.49.2.3.2.jpg',
+    mediumImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/fbd7dd260d736654532e6c0b1ec185a0cede8675.49.2.3.2.jpg?thum=54',
+    nickname: 'はぁぽじ',
+    pickup: 0,
+    rank: '3',
+    recipeCost: '300円前後',
+    recipeDescription: 'そのままでも、ご飯にのせて丼にしても♪',
+    recipeId: 1760028309,
+    recipeIndication: '約10分',
+    recipeMaterial: [
+      '鶏むね肉',
+      '塩',
+      '酒',
+      '片栗粉',
+      '○水',
+      '○塩',
+      '○鶏がらスープの素',
+      '○黒胡椒',
+      '長ネギ',
+      'いりごま',
+      'ごま油',
+    ],
+    recipePublishday: '2017/10/10 22:37:34',
+    recipeTitle: 'ご飯がすすむ！鶏むね肉のねぎ塩焼き',
+    recipeUrl: 'https://recipe.rakuten.co.jp/recipe/1760028309/',
+    shop: 0,
+    smallImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/fbd7dd260d736654532e6c0b1ec185a0cede8675.49.2.3.2.jpg?thum=55',
+  },
+  {
+    foodImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/3e5906a3607b2f1321cda1158b251c4223204420.40.2.3.2.jpg',
+    mediumImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/3e5906a3607b2f1321cda1158b251c4223204420.40.2.3.2.jpg?thum=54',
+    nickname: '*ももら*',
+    pickup: 1,
+    rank: '4',
+    recipeCost: '300円前後',
+    recipeDescription:
+      '鶏胸肉なのにウイング風♬骨が無いので食べ易くお弁当にもピッタリ♡油で揚げないのでヘルシーです♪',
+    recipeId: 1400014946,
+    recipeIndication: '約10分',
+    recipeMaterial: [
+      '鶏むね肉',
+      '片栗粉',
+      '塩コショウ',
+      '炒め用サラダ油',
+      'A醤油',
+      'Aみりん',
+      'Aお酒',
+      'A砂糖',
+    ],
+    recipePublishday: '2015/09/30 14:28:09',
+    recipeTitle: '鶏胸肉で簡単♪手羽風揚げない甘辛照焼チキンお弁当に',
+    recipeUrl: 'https://recipe.rakuten.co.jp/recipe/1400014946/',
+    shop: 0,
+    smallImageUrl:
+      'https://image.space.rakuten.co.jp/d/strg/ctrl/3/3e5906a3607b2f1321cda1158b251c4223204420.40.2.3.2.jpg?thum=55',
+  },
+]
+
+export default testData
