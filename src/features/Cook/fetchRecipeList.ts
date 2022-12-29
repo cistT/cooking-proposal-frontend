095577ec985514a392f17bc38b27e2eca1601df3
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 18:55:06 +0900

    envファイルから楽天APIの利用に必要なIDを取得できるように変更

--- src/features/Cook/fetchRecipeList.ts
+++ src/features/Cook/fetchRecipeList.ts
@@ -1,6 +1,6 @@
 const fetchRecipeList = async (categoryId: string) => {
   const res = await fetch(
-    `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=${process.env.APPLICATION_ID}&categoryId=${categoryId}`,
+    `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=${process.env.NEXT_PUBLIC_APPLICATION_ID}&categoryId=${categoryId}`,
   )
   const recipeList = res.json()
 
