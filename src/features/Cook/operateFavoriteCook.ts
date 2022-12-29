c90dc06c59d5eb51101bff96b79da4c071635359
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 18:57:08 +0900

    お気に入りレシピの登録・解除・取得をする関数を作成

---
+++ src/features/Cook/operateFavoriteCook.ts
@@ -0,0 +1,47 @@
+import Recipe from './Recipe'
+
+export const registerFavoriteRecipe = async (
+  userId: string,
+  recipe: Recipe,
+) => {
+  const url = `${
+    process.env.NEXT_PUBLIC_DATABASE_URL || 'http://localhost:8080'
+  }/favorite`
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({
+      userId,
+      ...recipe,
+      recipePublishday: new Date(recipe.recipePublishday),
+    }),
+  })
+}
+
+export const releaseFavoriteRecipe = async (
+  userId: string,
+  recipeId: number,
+) => {
+  const url = `${
+    process.env.NEXT_PUBLIC_DATABASE_URL || 'http://localhost:8080'
+  }/favorite/${userId}/${recipeId}`
+
+  const res = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  })
+}
+
+export const fetchFavoriteRecipes = async (userId: string) => {
+  const url = `${
+    process.env.NEXT_PUBLIC_DATABASE_URL || 'http://localhost:8080'
+  }/favorite/${userId}`
+
+  const res = await fetch(url)
+  const recipe = await res.json()
+  return recipe
+}
