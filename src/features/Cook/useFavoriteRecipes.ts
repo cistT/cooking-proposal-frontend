25157d7b7bbc3d62449850c2cf480d4f05214b51
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 18:57:28 +0900

    お気に入りレシピを取得するHooks

---
+++ src/features/Cook/useFavoriteRecipes.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+import Recipe from './Recipe'
+import { fetchFavoriteRecipes } from './operateFavoriteCook'
+
+import { UID } from 'features/Auth/UID'
+
+const useFavoriteRecipes = (uid: UID) => {
+  const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([])
+  const [finishedFetch, setFinishedFetch] = useState(false)
+  useEffect(() => {
+    ;(async () => {
+      if (uid) {
+        const recipe = await fetchFavoriteRecipes(uid)
+        setFavoriteRecipes(recipe)
+      }
+      setFinishedFetch(true)
+    })()
+  }, [uid])
+
+  return {
+    finishedFetch,
+    favoriteRecipes,
+  }
+}
+
+export default useFavoriteRecipes
