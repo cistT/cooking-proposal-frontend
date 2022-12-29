7a733d002e6b511f3deaf8e71c0a46a77dd45a6f
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:03:30 +0900

    ログイン情報を取得・子コンポーネントに供給できるように変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { createContext, useState } from 'react'
 import 'modern-css-reset/dist/reset.min.css'
 
 import Layout from 'components/Layout/Layout'
+import Loading from 'components/Loading/Loading'
+import UIDProvider from 'features/Auth/UIDProvider'
+import useGoogleAuth from 'features/Auth/useGoogleAuth'
 import { Contents } from 'features/News/News'
 
 export const NewsContentsContext = createContext<Contents[] | null>(null)
@@ -19,13 +22,23 @@ export default function App({ Component, pageProps }: AppProps) {
     setNewsContents(contents)
   }
 
+  const { uid, isInitializeAuth, login, logout } = useGoogleAuth()
+
   return (
-    <NewsContentsContext.Provider value={newsContents}>
-      <NewsContext.Provider value={temporarilySavedNewsContents}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </NewsContext.Provider>
-    </NewsContentsContext.Provider>
+    <>
+      {isInitializeAuth ? (
+        <NewsContentsContext.Provider value={newsContents}>
+          <NewsContext.Provider value={temporarilySavedNewsContents}>
+            <UIDProvider value={uid}>
+              <Layout uid={uid} login={login} logout={logout}>
+                <Component {...pageProps} />
+              </Layout>
+            </UIDProvider>
+          </NewsContext.Provider>
+        </NewsContentsContext.Provider>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
