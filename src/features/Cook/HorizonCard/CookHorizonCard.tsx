b4ed5aeb509a8a0ea17df19c3dffd5ef9a07bd1c
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:02:28 +0900

    楽天レシピに遷移するボタンとお気に入り登録・解除を行うボタンを追加

--- src/features/Cook/HorizonCard/CookHorizonCard.tsx
+++ src/features/Cook/HorizonCard/CookHorizonCard.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react'
 
+import FavoriteButton from '../FavoriteButton/FavoriteButton'
+import ShowRecipeButton from '../ShowRecipeButton/ShowRecipeButton'
+
+import styles from './CookHorizonCard.module.scss'
+
 import HorizonCard from 'components/Card/HorizonCard/HorizonCard'
 import CookOverview, {
   CookOverviewProps,
@@ -7,23 +12,50 @@ import CookOverview, {
 
 type CookHorizonCardProps = Readonly<{
   header: string
+  href: string
   image?: string
   imageAlt?: string
+  login?: boolean
+  registerFavoriteRecipe: () => void
+  releaseFavoriteRecipe: () => void
 }> &
   CookOverviewProps
 
 const CookHorizonCard: FC<CookHorizonCardProps> = (props) => {
-  const { description, indication, cost, material } = props
+  const {
+    description,
+    indication,
+    cost,
+    material,
+    login = true,
+    href,
+    registerFavoriteRecipe = () => undefined,
+    releaseFavoriteRecipe = () => undefined,
+  } = props
+
   return (
     <HorizonCard
       {...props}
       explanation={
-        <CookOverview
-          description={description}
-          indication={indication}
-          cost={cost}
-          material={material}
-        />
+        <>
+          <CookOverview
+            description={description}
+            indication={indication}
+            cost={cost}
+            material={material}
+          />
+
+          <div className={styles['group']}>
+            <ShowRecipeButton href={href} />
+            {login && (
+              <FavoriteButton
+                register={registerFavoriteRecipe}
+                release={releaseFavoriteRecipe}
+                initialFavorite
+              />
+            )}
+          </div>
+        </>
       }
     />
   )
