bc2ba7f30512fc9b5d14454f21e3e0e97890de19
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:04:15 +0900

    お気に入り登録したレシピを表示できるようにコンテンツを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,45 @@
 import { IconSearch } from '@tabler/icons'
 import { IconNews } from '@tabler/icons'
 import { IconAlertCircle } from '@tabler/icons'
+import { IconHeart } from '@tabler/icons'
+import { useContext } from 'react'
 
 import styles from './index.module.scss'
 
 import LinkTextCardWithIcon from 'components/Button/LinkTextCardWithIcon/LinkTextCardWithIcon'
+import {
+  CAUTION_PATH,
+  FAVORITE_RECIPES_PATH,
+  PROPOSAL_RECIPE_PATH,
+  SELECTION_NEWS_PATH,
+} from 'consts/path'
+import { UIDContext } from 'features/Auth/UIDProvider'
 
 const Home = () => {
+  const uid = useContext(UIDContext)
+
   return (
     <div className={styles['container']}>
       <LinkTextCardWithIcon
         text='料理を見る'
-        href='/proposal'
+        href={PROPOSAL_RECIPE_PATH}
         icon={<IconSearch className={styles['icon']} />}
       />
-
-      {/* ログイン機能及びデータベースを構築したのち追加
-      <LinkTextCardWithIcon
-        text='お気に入り'
-        href='/save'
-        icon={<IconStar className={styles['icon']} />}
-      /> */}
-
+      {uid && (
+        <LinkTextCardWithIcon
+          text='お気に入り'
+          href={FAVORITE_RECIPES_PATH}
+          icon={<IconHeart className={styles['icon']} />}
+        />
+      )}
       <LinkTextCardWithIcon
         text='お知らせ'
-        href='/news'
+        href={SELECTION_NEWS_PATH}
         icon={<IconNews className={styles['icon']} />}
       />
-
       <LinkTextCardWithIcon
         text='利用上の注意'
-        href='/caution'
+        href={CAUTION_PATH}
         icon={<IconAlertCircle className={styles['icon']} />}
       />
     </div>
