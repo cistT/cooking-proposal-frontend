1473b7db422748dc6e12018c8d99bdb6f2372627
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:05:55 +0900

    SSRでレシピを取得するのではなく、クライアント側で取得できるように変更

--- src/pages/proposal/index.tsx
+++ src/pages/proposal/index.tsx
@@ -1,65 +1,55 @@
-import { GetServerSideProps } from 'next'
-import React, { FC } from 'react'
+import React, { useContext } from 'react'
 
 import styles from './index.module.scss'
 
 import BackButton from 'components/Button/BackButton/BackButton'
+import Loading from 'components/Loading/Loading'
+import { UIDContext } from 'features/Auth/UIDProvider'
 import CookVerticalCard from 'features/Cook/VerticalCard/CookVerticalCard'
-import fetchRecipeList from 'features/Cook/fetchRecipeList'
-import getRandomRecipeCategoryID from 'features/Cook/getRandomRecipeCategoryID'
+import {
+  registerFavoriteRecipe,
+  releaseFavoriteRecipe,
+} from 'features/Cook/operateFavoriteCook'
+import useRecipe from 'features/Cook/useRecipe'
 
-type ProposalProps = {
-  recipe: {
-    foodImageUrl: string
-    mediumImageUrl: string
-    nickname: string
-    pickup: number
-    rank: string
-    recipeCost: string
-    recipeDescription: string
-    recipeId: number
-    recipeIndication: string
-    recipeMaterial: string[]
-    recipePublishday: string
-    recipeTitle: string
-    recipeUrl: string
-    shop: number
-    smallImageUrl: string
-  }
-}
-
-const Index: FC<ProposalProps> = (props) => {
-  const { recipe } = props
+const Recipe = () => {
+  const { recipe, finishedFetch } = useRecipe()
+  const uid = useContext(UIDContext)
 
   return (
-    <div className={styles['container']}>
-      <CookVerticalCard
-        header={recipe.recipeTitle}
-        image={recipe.smallImageUrl}
-        imageAlt={recipe.recipeTitle}
-        description={recipe.recipeDescription}
-        indication={recipe.recipeIndication}
-        cost={recipe.recipeCost}
-        material={recipe.recipeMaterial}
-        href={recipe.recipeUrl}
-      />
-      <BackButton />
-    </div>
+    <>
+      {finishedFetch ? (
+        <div className={styles['container']}>
+          {recipe && (
+            <CookVerticalCard
+              header={recipe.recipeTitle}
+              image={recipe.smallImageUrl}
+              imageAlt={recipe.recipeTitle}
+              description={recipe.recipeDescription}
+              indication={recipe.recipeIndication}
+              cost={recipe.recipeCost}
+              material={recipe.recipeMaterial}
+              href={recipe.recipeUrl}
+              login={uid ? true : false}
+              registerFavoriteRecipe={() => {
+                if (uid) {
+                  registerFavoriteRecipe(uid, recipe)
+                }
+              }}
+              releaseFavoriteRecipe={() => {
+                if (uid) {
+                  releaseFavoriteRecipe(uid, recipe.recipeId)
+                }
+              }}
+            />
+          )}
+          <BackButton />
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
 
-export default Index
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const categoryID = getRandomRecipeCategoryID()
-  const recipeList = await fetchRecipeList(categoryID)
-
-  const recipe =
-    recipeList.result[Math.floor(Math.random() * recipeList.result.length)]
-
-  return {
-    props: {
-      recipe,
-    },
-  }
-}
+export default Recipe
