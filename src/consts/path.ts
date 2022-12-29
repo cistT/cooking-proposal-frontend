11beac971dc477baf7da69128e1fd422bf3a416c
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:23:22 +0900

    レシピを表示するパスを変更

--- src/consts/path.ts
+++ src/consts/path.ts
@@ -1,4 +1,4 @@
-export const PROPOSAL_RECIPE_PATH = '/proposal' as const
+export const PROPOSAL_RECIPE_PATH = '/recipe' as const
 export const FAVORITE_RECIPES_PATH = '/favorite' as const
 export const SELECTION_NEWS_PATH = '/news' as const
 export const CAUTION_PATH = '/caution' as const
