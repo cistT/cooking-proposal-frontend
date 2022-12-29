12a590b14f1945bcf67a4af57227d4e18eb26e5b
cistT <cist.b220.t.chitose@gmail.com>
2022-12-29 19:01:44 +0900

    画像の大きさの調整とカードを押しても楽天レシピに遷移しないように変更

--- src/components/Card/HorizonCard/HorizonCard.tsx
+++ src/components/Card/HorizonCard/HorizonCard.tsx
@@ -5,30 +5,25 @@ import CardProps from '../Card'
 
 import styles from './HorizonCard.module.scss'
 
-import NoneUnderlineLink from 'components/Link/NoneUnderlineLink'
-
 export type HorizonCardProps = CardProps &
   Readonly<{
-    href?: string
     image?: string
     imageAlt?: string
   }>
 
 const HorizonCard: FC<HorizonCardProps> = (props) => {
-  const { header, explanation, href = '', image, imageAlt } = props
+  const { header, explanation, image, imageAlt } = props
 
   return (
-    <NoneUnderlineLink href={href}>
-      <Card radius='md' withBorder>
-        <Group className={styles['group']}>
-          <Image src={image} alt={imageAlt} className={styles['image']} />
-          <Title order={1} className={styles['header']}>
-            {header}
-          </Title>
-          <div className={styles['explanation']}>{explanation}</div>
-        </Group>
-      </Card>
-    </NoneUnderlineLink>
+    <Card radius='md' withBorder>
+      <Group className={styles['group']}>
+        <Image src={image} alt={imageAlt} className={styles['image']} />
+        <Title order={1} className={styles['header']}>
+          {header}
+        </Title>
+        <div className={styles['explanation']}>{explanation}</div>
+      </Group>
+    </Card>
   )
 }
 
