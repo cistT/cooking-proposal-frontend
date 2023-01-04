e9b6aebc7e4fe704e9351ba212f72af516bfab6a
cistT <cist.b220.t.chitose@gmail.com>
2023-01-04 15:54:09 +0900

    API GatewayのエンドポイントとAPIキーを追加

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,1 +1,7 @@
 export const APP_NAME = '料理提案アプリ(仮)' as const
+
+export const COOK_PROPOSAL_API_URL = process.env
+  .NEXT_PUBLIC_COOK_PROPOSAL_API_URL as string
+
+export const COOK_PROPOSAL_API_KEY = process.env
+  .NEXT_PUBLIC_COOK_PROPOSAL_API_KEY as string
