c0640378cef195da9125344ff1bac4e425a73cdc
cistT <cist.b220.t.chitose@gmail.com>
2023-01-04 16:36:06 +0900

    API Routesからデータを取得するように変更

--- src/features/Recipe/operateFavoriteRecipe.ts
+++ src/features/Recipe/operateFavoriteRecipe.ts
@@ -1,20 +1,13 @@
-import { COOK_PROPOSAL_API_KEY } from './../../consts/app'
 import Recipe from './Recipe'
-
-import { COOK_PROPOSAL_API_URL } from 'consts/app'
 
 export const registerFavoriteRecipe = async (
   userId: string,
   recipe: Recipe,
 ) => {
-  const url = `${COOK_PROPOSAL_API_URL || 'http://localhost:8080'}/favorite`
+  const url = '/api/favorite'
 
   await fetch(url, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      'x-api-key': COOK_PROPOSAL_API_KEY,
-    },
     body: JSON.stringify({
       userId,
       ...recipe,
@@ -27,29 +20,15 @@ export const releaseFavoriteRecipe = async (
   userId: string,
   recipeId: number,
 ) => {
-  const url = `${
-    COOK_PROPOSAL_API_URL || 'http://localhost:8080'
-  }/favorite/${userId}/${recipeId}`
-
+  const url = `/api/favorite/${userId}/${recipeId}`
   await fetch(url, {
     method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      'x-api-key': COOK_PROPOSAL_API_KEY,
-    },
   })
 }
 
 export const fetchFavoriteRecipes = async (userId: string) => {
-  const url = `${
-    COOK_PROPOSAL_API_URL || 'http://localhost:8080'
-  }/favorite/${userId}`
-
-  const res = await fetch(url, {
-    headers: {
-      'x-api-key': COOK_PROPOSAL_API_KEY,
-    },
-  })
+  const url = `api/favorite/${userId}`
+  const res = await fetch(url)
   const recipe = await res.json()
   return recipe
 }
