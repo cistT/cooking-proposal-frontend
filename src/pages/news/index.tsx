1a98174723850f5bf147aadc81467f1a69232ae6
cistT <cist.b220.t.chitose@gmail.com>
2023-01-05 11:21:41 +0900

    API Routes経由でお知らせを取得するように変更

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,54 +1,48 @@
 import { Text } from '@mantine/core'
-import React, { FC, useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 
 import styles from './index.module.scss'
 
 import BackButton from 'components/Button/BackButton/BackButton'
-import { FetchedNews } from 'features/News/News'
+import Loading from 'components/Loading/Loading'
 import NewsSelectCard from 'features/News/Select/NewsSelectCard'
-import fetchNews from 'features/News/fetchNews'
+import useFetchNews from 'features/News/useFetchNews'
 import { NewsContext } from 'pages/_app'
 
-type NewsSelectProps = FetchedNews
+const NewsSelect = () => {
+  const { news, finishedFetch } = useFetchNews()
 
-const NewsSelect: FC<NewsSelectProps> = (props) => {
-  const { contents } = props
-  const temporarilySavedNewsContents = useContext(NewsContext)
+  const temporarilySavedNewsnews = useContext(NewsContext)
   useEffect(() => {
-    temporarilySavedNewsContents(contents)
-  }, [contents, temporarilySavedNewsContents])
+    temporarilySavedNewsnews(news)
+  }, [news, temporarilySavedNewsnews])
 
   return (
     <div className={styles['container']}>
       <Text className={styles['text']}>お知らせ</Text>
-      <>
-        {contents.length === 0 && (
-          <Text className={styles['text']}>現在、お知らせはありません。</Text>
-        )}
-        {contents.map((data) => (
-          <NewsSelectCard
-            key={data.id}
-            id={data.id}
-            title={data.title}
-            category={data?.category?.name}
-            date={new Date(data.publishedAt)}
-          />
-        ))}
-      </>
+
+      {finishedFetch ? (
+        <>
+          {news.length === 0 && (
+            <Text className={styles['text']}>現在、お知らせはありません。</Text>
+          )}
+          {news.map((data) => (
+            <NewsSelectCard
+              key={data.id}
+              id={data.id}
+              title={data.title}
+              category={data?.category?.name}
+              date={new Date(data.publishedAt)}
+            />
+          ))}
+        </>
+      ) : (
+        <Loading />
+      )}
+
       <BackButton />
     </div>
   )
 }
 
 export default NewsSelect
-
-export const getServerSideProps = async () => {
-  const news = await fetchNews()
-  const contents = news.contents
-
-  return {
-    props: {
-      contents,
-    },
-  }
-}
