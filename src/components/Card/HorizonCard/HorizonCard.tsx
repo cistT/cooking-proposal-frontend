a3562edabf21f8205949ba3ccb0ebd3ccbcb4ea0
cistT <cist.b220.t.chitose@gmail.com>
2023-01-06 15:53:48 +0900

    レシピの画像及びレシピの概要の下にボタンを設置できるように変更

--- src/components/Card/HorizonCard/HorizonCard.tsx
+++ src/components/Card/HorizonCard/HorizonCard.tsx
@@ -1,5 +1,5 @@
 import { Card, Group, Image, Title } from '@mantine/core'
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 
 import CardProps from '../Card'
 
@@ -9,10 +9,11 @@ export type HorizonCardProps = CardProps &
   Readonly<{
     image?: string
     imageAlt?: string
+    underContents?: ReactNode
   }>
 
 const HorizonCard: FC<HorizonCardProps> = (props) => {
-  const { header, explanation, image, imageAlt } = props
+  const { header, explanation, image, imageAlt, underContents } = props
 
   return (
     <Card radius='md' withBorder>
@@ -22,6 +23,7 @@ const HorizonCard: FC<HorizonCardProps> = (props) => {
           {header}
         </Title>
         <div className={styles['explanation']}>{explanation}</div>
+        <div className={styles['under-contents']}>{underContents}</div>
       </Group>
     </Card>
   )
