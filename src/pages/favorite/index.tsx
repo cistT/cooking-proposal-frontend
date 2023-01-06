fdd7aff20ed842384abff5bb42d2466572a6a75c
cistT <cist.b220.t.chitose@gmail.com>
2023-01-06 15:55:16 +0900

    スマートフォンかどうかで、レシピの表示形式を変更できるように修正

--- src/pages/favorite/index.tsx
+++ src/pages/favorite/index.tsx
@@ -7,16 +7,69 @@ import BackButton from 'components/Button/BackButton/BackButton'
 import Loading from 'components/Loading/Loading'
 import { UIDContext } from 'features/Auth/UIDProvider'
 import RecipeHorizonCard from 'features/Recipe/HorizonCard/RecipeHorizonCard'
+import RecipeVerticalCard from 'features/Recipe/VerticalCard/RecipeVerticalCard'
 import {
   registerFavoriteRecipe,
   releaseFavoriteRecipe,
 } from 'features/Recipe/operateFavoriteRecipe'
 import useFavoriteRecipes from 'features/Recipe/useFavoriteRecipes'
+import useIsSP from 'hooks/useIsSP'
 
 const Index = () => {
   const uid = useContext(UIDContext)
 
   const { finishedFetch, favoriteRecipes } = useFavoriteRecipes(uid)
+  const { isSP } = useIsSP()
+
+  if (isSP) {
+    return (
+      <>
+        {finishedFetch ? (
+          <div className={styles['container']}>
+            <Text className={styles['text']}>お気に入りレシピ</Text>
+
+            {favoriteRecipes.length === 0 && (
+              <Text className={styles['text']}>
+                お気に入りレシピは登録されていません
+              </Text>
+            )}
+
+            <>
+              {favoriteRecipes.map((recipe) => (
+                <RecipeVerticalCard
+                  key={recipe.recipeId}
+                  header={recipe.recipeTitle}
+                  image={recipe.smallImageUrl}
+                  imageAlt={recipe.recipeTitle}
+                  description={recipe.recipeDescription}
+                  indication={recipe.recipeIndication}
+                  cost={recipe.recipeCost}
+                  material={recipe.recipeMaterial}
+                  href={recipe.recipeUrl}
+                  registerFavoriteRecipe={() => {
+                    if (uid) {
+                      registerFavoriteRecipe(uid, recipe)
+                    }
+                  }}
+                  releaseFavoriteRecipe={() => {
+                    if (uid) {
+                      releaseFavoriteRecipe(uid, recipe.recipeId)
+                    }
+                  }}
+                  login
+                  initialFavorite
+                />
+              ))}
+            </>
+
+            <BackButton />
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </>
+    )
+  }
 
   return (
     <>
