d6ba635df16f42bebdd0d8cd91e18b2007517686
cistT <cist.b220.t.chitose@gmail.com>
2023-02-08 18:34:35 +0900

    画面の横幅が変わるごとに、レスポンシブにするべきかどうかを判断

--- src/hooks/useIsSP.ts
+++ src/hooks/useIsSP.ts
@@ -1,11 +1,16 @@
+import { useEffect, useState } from 'react'
 import { useWindowSize } from 'react-use'
 
 const SMARTPHONE_MAX_WIDTH = 600
 
 const useIsSP = () => {
   const { width } = useWindowSize()
+  const [isSP, setIsSP] = useState(width <= SMARTPHONE_MAX_WIDTH)
+  useEffect(() => {
+    setIsSP(width <= SMARTPHONE_MAX_WIDTH)
+  }, [width])
 
-  return { isSP: width <= SMARTPHONE_MAX_WIDTH }
+  return { isSP }
 }
 
 export default useIsSP
