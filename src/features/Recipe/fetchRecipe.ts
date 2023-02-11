4005c0019194d7ab0ae77ff75da26c39b60a5819
cistT <cist.b220.t.chitose@gmail.com>
2023-02-11 20:24:40 +0900

    フォルダの変更と不要なファイルの削除

--- src/features/Recipe/fetchRecipe.ts
+++
@@ -1,9 +0,0 @@
-const fetchRecipe = async () => {
-  const url = '/api/recipe/random'
-  const res = await fetch(url)
-  const recipe = await res.json()
-
-  return recipe
-}
-
-export default fetchRecipe
