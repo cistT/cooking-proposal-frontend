8d5c9f1021ca7c50a7415057b518a51cda6a5e52
cistT <cist.b220.t.chitose@gmail.com>
2023-02-12 20:10:51 +0900

    tailwindの読み込みを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from 'next/app'
 
-
 import 'modern-css-reset/dist/reset.min.css'
-import 'tailwindcss/tailwind.css';
 
 import Layout from 'components/Layout/Layout'
 import Loading from 'components/Loading/Loading'
