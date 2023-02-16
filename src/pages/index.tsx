e03e84def82828d18fc4679d8f64b46f19462274
cistT <cist.b220.t.chitose@gmail.com>
2023-02-16 17:47:52 +0900

    見づらかったため、レシピを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,63 +9,12 @@ import styles from './index.module.scss'
 import LinkTextCardWithIcon from 'components/Button/LinkTextCardWithIcon/LinkTextCardWithIcon'
 import path from 'consts/path'
 import { UIDContext } from 'features/Auth'
-import {
-  useFetchRandomRecipe,
-  useRegisterFavoriteRecipe,
-  useReleaseFavoriteRecipe,
-} from 'features/Recipe'
-import SimpleVerticalCard from 'features/Recipe/components/SimpleVerticalCard/SimpleVerticalCard'
-import Loading from 'components/Loading/Loading'
 
 const Home = () => {
   const uid = useContext(UIDContext)
 
-  const { recipe, isLoading } = useFetchRandomRecipe()
-  const { registerFavoriteRecipe } = useRegisterFavoriteRecipe()
-  const { releaseFavoriteRecipe } = useReleaseFavoriteRecipe()
-
-  if (isLoading) {
-    return <Loading />
-  }
-
-  if (!recipe) {
-    return <p>データが取得できませんでした。</p>
-  }
-
   return (
     <div className={styles['container']}>
-      <SimpleVerticalCard
-        title={recipe.title}
-        imageURL={recipe.image}
-        imageAlt={recipe.title}
-        recipeURL={recipe.url}
-        login={uid ? true : false}
-        registerFavoriteRecipe={() => {
-          if (uid) {
-            registerFavoriteRecipe.mutate({
-              userID: uid,
-              recipe: {
-                id: recipe.id,
-                title: recipe.title,
-                url: recipe.url,
-                image: recipe.image,
-                cost: recipe.cost,
-                description: recipe.description,
-                indication: recipe.indication,
-                material: recipe.material,
-              },
-            })
-          }
-        }}
-        releaseFavoriteRecipe={() => {
-          if (uid) {
-            releaseFavoriteRecipe.mutate({
-              userID: uid,
-              recipeID: recipe.id,
-            })
-          }
-        }}
-      />
       <LinkTextCardWithIcon
         text='料理を見る'
         href={path.PROPOSAL_RECIPE_PATH}
